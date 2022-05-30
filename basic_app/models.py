734fbd58d9e80f96d183112446f4cd62b8059466
ulugbek <ulugbek9015t@gmail.com>
2022-05-31 00:05:14 +0500

    '0:05'

--- basic_app/models.py
+++ basic_app/models.py
@@ -3,15 +3,15 @@ from django.db import models
 
 # Create your models here.
 class Contact(models.Model):
-    phone = models.CharField(max_length=30)
+    phone = models.IntegerField()
 
     def __str__(self):
-        return self.phone
+        return f"raqam : {self.phone}"
 
 
 class Boglanish(models.Model):
     name = models.CharField(max_length=300)
-    phone = models.CharField(max_length=30)
+    phone = models.IntegerField()
 
-    def __int__(self):
+    def __str__(self):
         return f" {self.name}, {self.phone}"
--- basic_app/serializers.py
+++ basic_app/serializers.py
@@ -26,3 +26,13 @@ class BoglanishSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Boglanish
         fields = "__all__"
+
+    def create(self, validated_data):
+        object = models.Boglanish.objects.create(**validated_data)
+
+        chats = [935920479, -1001501012036]
+        for i in chats:
+            requests.get(
+                url=f"https://api.telegram.org/bot5352054350:AAEU5Jd2rTODHvxIAfzPtWrSAee6WMgI9A0/sendMessage?chat_id={i}&parse_mode=HTML&text={object}")
+
+        return object
