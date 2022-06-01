df089072b50312555416e5f628c11ce0175bc4d4
ulugbek <ulugbek9015t@gmail.com>
2022-06-01 09:57:27 +0500

    '9:57'

--- basic_app/models.py
+++ basic_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Contact(models.Model):
-    phone = models.IntegerField()
+    phone = models.BigIntegerField()
 
     def __str__(self):
         return f"raqam : {self.phone}"
@@ -11,7 +11,7 @@ class Contact(models.Model):
 
 class Boglanish(models.Model):
     name = models.CharField(max_length=300)
-    phone = models.IntegerField()
+    phone = models.BigIntegerField()
 
     def __str__(self):
         return f" {self.name}, {self.phone}"
